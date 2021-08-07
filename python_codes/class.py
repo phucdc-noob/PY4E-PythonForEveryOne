17d96a1dc0a8e5fa4a710464bcd5283ab82ca093
PhucBonTu <82533607+phucdc-noob@users.noreply.github.com>
2021-08-07 22:35:35 +0700

    change something

--- python_codes/class.py
+++ python_codes/class.py
@@ -18,7 +18,42 @@ print("List students before sorting:\n%5s %5s %5s %5s %5s" % ("Name", "|", "Age"
 for student in students:
     print( "%5s %5s %5d %5s %5.1f" % ( student.name, "|", student.age, "|", student.testScore) )
 
-print("List students after sorting:\n%5s %5s %5s %5s %5s" % ("Name", "|", "Age", "|", "Test score"))
+# sort by name
+print("List students after sorting by name:\n%5s %5s %5s %5s %5s" % ("Name", "|", "Age", "|", "Test score"))
 students.sort(key = lambda x: x.name)
 for student in students:
     print( "%5s %5s %5d %5s %5.1f" % (student.name, "|", student.age, "|", student.testScore) )
+
+# sort by name, if 2 students have same name, sort by their test score
+print("List students after sorting by name and test score:\n%5s %5s %5s %5s %5s" % ("Name", "|", "Age", "|", "Test score"))
+students.sort(key = lambda x: (x.name, x.testScore))
+for student in students:
+    print( "%5s %5s %5d %5s %5.1f" % (student.name, "|", student.age, "|", student.testScore) )
+
+# example:
+'''
+Enter name: Phuc
+Enter age: 21
+Enter test score: 3.2
+Enter name: Phuc
+Enter age: 21
+Enter test score: 2.6
+Enter name: Hoang
+Enter age: 21
+Enter test score: 8.0
+List students before sorting:
+ Name     |   Age     | Test score
+ Phuc     |    21     |   3.2
+ Phuc     |    21     |   2.6
+Hoang     |    21     |   8.0
+List students after sorting by name:
+ Name     |   Age     | Test score
+Hoang     |    21     |   8.0
+ Phuc     |    21     |   3.2
+ Phuc     |    21     |   2.6
+List students after sorting by name and test score:
+ Name     |   Age     | Test score
+Hoang     |    21     |   8.0
+ Phuc     |    21     |   2.6
+ Phuc     |    21     |   3.2
+'''
--- python_codes/selectionSort2Array.py
+++
@@ -1,8 +0,0 @@
-arr = [int(num) for num  in input().split()]
-
-sArr = [] #sorted array
-
-for i in range(len(arr)):
-    sArr.append(min(arr))
-    arr.remove(min(arr))
-print(*sArr, sep="\n")
