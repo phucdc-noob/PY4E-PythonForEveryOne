deb7bfcfd339874577b5e4c7b7f98540ca20387f
PhucBonTu <82533607+phucdc-noob@users.noreply.github.com>
2021-08-11 21:50:39 +0700

    Update factorial.py

--- python_codes/factorial.py
+++ python_codes/factorial.py
@@ -1,5 +1,5 @@
 # Using python math module to calculate factorial
-# From python3, math module's functions are implemented from C, so it solve this problem really fast with integer numbers [0; 2147483647]
+# From python3, math module's functions are implemented from C, so it solve this problem really fast with integer numbers [1; 2147483647]
 import math
 
 n = int(input())
